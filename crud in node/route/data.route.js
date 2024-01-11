98dff854297dc3c66fcc4cc2a6a5c596e0604e23
Tarun Kumar <tarunmehto71@gmail.com>
2024-01-11 08:25:16 +0530

    Update data.route.js

--- crud in node/route/data.route.js
+++ crud in node/route/data.route.js
@@ -1,64 +1,73 @@
+const express = require("express");
+const { DataModel } = require("../model/data.model");
 
-const express = require("express")
+const DataRouter = express.Router();
 
-const DataRouter  =  express.Router()
-
-const {DataModel} = require("../model/data.model")
-
-
-
-//get data from database (show all data in given collection)
+// Get data from the database (show all data in the given collection)
 DataRouter.get("/", async (req, res) => {
-    let user = await DataModel.find();
-    res.send(user);
-  });
-  
-  //post data from database (post - post the data by give data in req.body)
-  
-  DataRouter.post("/", async (req, res) => {
-    const payload = req.body;
-    // console.log(payload)
-  
-    //   const postData = await DataModel.insertMany([payload])
-    //     res.send(postData)
-  
-    const user = new DataModel(payload);
-    await user.save();
-  
-    res.send({ msg: "Data is posted" });
-  });
-  
-  //put data from database (put - use for change whole data object)
-  
-  DataRouter.put("/:id", async (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const payload = req.body;
-  
-    const user = await DataModel.findByIdAndUpdate({ _id: id }, payload);
-    res.send({ msg: "whole data Update(put)" });
-  });
-  
-  //patch data from database (patch - use for change one element of object )
-  
-  DataRouter.patch("/:id", async (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const payload = req.body;
-  
-    const user = await DataModel.findByIdAndUpdate({ _id: id }, payload);
-    res.send({ msg: "specific data element Updated (patch)" });
-  });
-  
-  //delete data from database (delete - delete whole object by given id )
-  
-  DataRouter.delete("/:id", async (req, res) => {
-    const id = req.params.id;
-    const user = await DataModel.findByIdAndDelete({ _id: id });
-  
-    res.send({ msg: "Data is deleted by given id" });
-  });
+    try {
+        let users = await DataModel.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(`Internal Server Error${error.message}`);
+    }
+});
 
+// Post data to the database (post - post the data by giving data in req.body)
+DataRouter.post("/", async (req, res) => {
+    try {
+        const payload = req.body;
+        const user = new DataModel(payload);
+        await user.save();
+        res.status(200).send( "Data is posted" );
+    } catch (error) {
+        res.status(500).send(`Internal Server Error${error.message}`);
+    }
+});
 
-  module.exports={DataRouter}
-  
+// Put data to the database (put - use for changing the whole data object)
+DataRouter.put("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const payload = req.body;
+        const user = await DataModel.findByIdAndUpdate({ _id: id }, payload);
+        if (!user) {
+            return res.status(400).send({ msg: "Data not found" });
+        }
+        res.status(200).send({ msg: "Whole data updated (put)" });
+    } catch (error) {
+        res.status(500).send(`Internal Server Error${error.message}`);
+    }
+});
+
+// Patch data in the database (patch - use for changing one element of the object)
+DataRouter.patch("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const payload = req.body;
+        const user = await DataModel.findByIdAndUpdate({ _id: id }, payload);
+        if (!user) {
+            return res.status(400).send( "Data not found" );
+        }
+        res.status(200).send("Specific data element updated (patch)" );
+    } catch (error) {
+        res.status(500).send(`Internal Server Error${error.message}`);
+    }
+});
+
+// Delete data from the database (delete - delete the whole object by the given id)
+DataRouter.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await DataModel.findByIdAndDelete({ _id: id });
+        if (!user) {
+            return res.status(400).send( "Data not found" );
+        }
+        res.status(200).send( "Data is deleted by the given id" );
+    } catch (error) {
+        
+        res.status(500).send(`Internal Server Error${error.message}`);
+    }
+});
+
+module.exports = { DataRouter };
